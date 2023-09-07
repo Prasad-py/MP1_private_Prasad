aa5f47385787f5d95bf7b48f381c0a8da6e07edf
Prasad-py <prasadgole2000@gmail.com>
2023-09-08 01:15:26 +0530

    new

--- example.py
+++ example.py
@@ -1,4 +1,4 @@
-import metapy
+# import metapy
 
 # def tokens_lowercase(doc):
 #     #Write a token stream that tokenizes with ICUTokenizer (use the argument "suppress_tags=True"), 
@@ -14,32 +14,60 @@ import metapy
 #         tokens.append(token)
 #     return tokens
 
+
+# if __name__ == '__main__':
+#     doc = metapy.index.Document()
+#     doc.content("I said that I can't believe that it only costs $19.95! I could only find it for more than $30 before.")
+#     print(doc.content()) #you can access the document string with .content()
+#     tokens = tokens_lowercase(doc)
+#     print(tokens)
+
+
+import metapy
+
 def tokens_lowercase(doc):
+    # Replace the comment below with a brief description of what this function does.
+    '''
+    This function tokenizes the input document using the ICUTokenizer with the argument "suppress_tags=True,"
+    converts tokens to lowercase, removes words with less than 2 and more than 5 characters, performs stemming,
+    and generates trigrams.
+    '''
+
     # Create an ICUTokenizer with suppress_tags=True
     tok = metapy.analyzers.ICUTokenizer(suppress_tags=True)
     tok.set_content(doc.content())
-
+    
     # Create a LengthFilter to remove words with less than 2 and more than 5 characters
     tok = metapy.analyzers.LengthFilter(tok, min=2, max=5)
-
+    
     # Create a Porter2Filter for stemming
     tok = metapy.analyzers.Porter2Filter(tok)
-
+    
     # Create an NGramWordAnalyzer for trigrams
     ana = metapy.analyzers.NGramWordAnalyzer(3, tok)
-
+    
     # Analyze the document to produce trigrams
     trigrams = ana.analyze(doc)
-
+    
     # Extract tokens from the trigrams
     tokens = [token for token, _ in trigrams]
-
+    
     return tokens
-
-
+    
 if __name__ == '__main__':
     doc = metapy.index.Document()
     doc.content("I said that I can't believe that it only costs $19.95! I could only find it for more than $30 before.")
-    print(doc.content()) #you can access the document string with .content()
+    
+    # Replace the comment below with an explanation of what the following code block does.
+    '''
+    This code block prints the original content of the document and then calls the tokens_lowercase function
+    to tokenize, lowercase, filter, stem, and generate trigrams from the content. It then prints the resulting tokens.
+    '''
+    
+    print("Original Document Content:")
+    print(doc.content())
+    
     tokens = tokens_lowercase(doc)
+    
+    print("\nProcessed Tokens:")
     print(tokens)
